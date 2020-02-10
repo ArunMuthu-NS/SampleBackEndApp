896851ca6adf4e6cf7fa21804378cb00f11019bd
Arunkumar Muthusamy <arunkumar.muthusamy@metapack.com>
2020-02-10 17:35:01 +0530

    Init Commit with sample maven project

---
+++ src/main/java/com/example/SampleBackEnd/SampleBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.SampleBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SampleBackEnd/SampleBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SampleBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
